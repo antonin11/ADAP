fe08abdb8bd55ecf70c6aceb7aaa263d0ce62561
Alfa59 <Antoine@MacBook-Pro-de-Toine.local>
2015-01-06 18:57:19 +0100

    Remise à zéro

--- src/Test.java
+++ src/Test.java
@@ -4,15 +4,10 @@ public class Test {
 	public static void main (String [] args){
 		
 
-		// Fils de ta mere ! :D
-		//Coucoucouocuocuocucoucoucoucoucoucocuocucoucoucoucocucoucoucocuocucoucoucouc
-		// Salut 
-<<<<<<< Updated upstream
-		// gestion des conflits , cela fonctionne t'il ?
-=======
-		//etzrxyctfvygbuhnjk
->>>>>>> Stashed changes
+	
 		
 	}
+	
+	
 	
 }
